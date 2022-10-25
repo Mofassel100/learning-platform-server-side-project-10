34f6bdf78de08e528f9b4d9f631b2be8772d54cb
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-10-25 23:32:55 +0600

    data loaded

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 const port = process.env.port || 5000;
-
+const learningData =require('./data/learningData.json')
+app.get  ('/',(req,res)=>{
+    res.send(learningData)
+})
+app.get  ('/homepage',(req,res)=>{
+    res.send(learningData)
+})
+app.get  ('/homepage/:id',(req,res)=>{
+    const id = req.params.id;
+    const lend = learningData?.find(L =>L.id===id)
+    res.send(lend)
+})
 
 app.listen(port,()=>{
     console.log('server is running ',port);
