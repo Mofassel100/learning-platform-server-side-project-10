16e02cd5fcc1ed07f67bee416ee77408897840b9
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-10-27 19:48:00 +0600

    moduil export

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const port = process.env.port || 5000;
+const port = process.env.port || 4000;
 const learningData =require('./data/learningData.json')
 app.get  ('/',(req,res)=>{
     res.send(learningData)
@@ -19,3 +19,4 @@ app.get  ('/homepage/:id',(req,res)=>{
 app.listen(port,()=>{
     console.log('server is running ',port);
 })
+module.exports=app;
